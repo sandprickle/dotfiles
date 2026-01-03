b4b14f9352b88f364c7a620eb3731cb7e29ce85f
Bryce Miller <sandprickle@users.noreply.github.com>
2026-01-02 21:25:58 -0500

    more fish cleanup

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,7 +1,7 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
+set -l __cachyos_fish_config "/usr/share/cachyos-fish-config/cachyos-config.fish"
 
-# overwrite greeting
-# potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
+if test -e $__cachyos_fish_config
+    source $__cachyos_fish_config
+end
+
+set -e __cachyos_fish_config
