4419f6999729a46917235a853c3624a9b8e2eef8
Bryce Miller <sandprickle@users.noreply.github.com>
2026-06-24 18:22:02 -0400

    updates

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -5,3 +5,16 @@ if test -e $__cachyos_fish_config
 end
 
 set -e __cachyos_fish_config
+
+if test -e $(which starship)
+    starship init fish | source
+end
+
+alias ls "eza -la"
+alias ll "eza -l"
+alias la "eza -a"
+alias ga "git add"
+alias gc "git commit"
+alias gcmsg "git commit -m"
+alias gcam "git commit -all -m"
+alias gp "git push"
